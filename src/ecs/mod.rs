28b005ac09c39ef7a468fba1cec1344a40b6de70
TeaSpillStudios <teaspillstudios@proton.me>
2022-11-20 23:06:20 +0000

    Add the code to use all of these together

---
+++ src/ecs/mod.rs
@@ -0,0 +1,13 @@
+mod world;
+mod system;
+mod entity;
+mod entity_manager;
+mod component;
+mod component_manager;
+
+pub use world::*;
+pub use system::*;
+pub use entity::*;
+pub use entity_manager::*;
+pub use component::*;
+pub use component_manager::*;
